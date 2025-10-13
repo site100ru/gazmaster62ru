86f9df25cd0aefc7d3842245cf380f7768151491
Roman Vasilyev <vasilyev-r@mail.ru>
2025-10-13 15:16:48 +0300

    Update single-service.php

--- single-service.php
+++ single-service.php
@@ -16,11 +16,11 @@
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<h1 class="mb-3"><?php echo wp_get_document_title(); ?></h1>
+				<h1 class="mb-3"><?php wp_title( "", true ); ?></h1>
 				<div class="breadcrumbs mb-4">
 					<a href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/img/ico/home-breadcrumbs.png"></a>
 					<span class="mx-1">/</span>
-					<span><?php echo wp_get_document_title(); ?></span>
+					<span><?php wp_title( "", true ); ?></span>
 				</div>
 				
 				
